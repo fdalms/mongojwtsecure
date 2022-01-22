9cc97fe119754080e7d9325568452b2413108393
fdalms <34750906+fdalms@users.noreply.github.com>
2022-01-23 01:00:31 +0300

    init

---
+++ src/main/java/com/fdalms/mongojwtsecure/MongojwtsecureApplication.java
@@ -0,0 +1,13 @@
+package com.fdalms.mongojwtsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongojwtsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongojwtsecureApplication.class, args);
+	}
+
+}
